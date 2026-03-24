54ef774ff756f1f439d54303f288625a545fe5c9
kimyee1 <neloezeagu@gmail.com>
2026-03-25 00:18:01 +0100

    remove DAI

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -3,7 +3,7 @@
 // e.g. clicking "USDC" shows only USDC whale trades
 
 // These are the filter options we show as buttons
-const TOKENS = ["all", "USDC", "WETH", "DAI", "cbETH", "BRETT"]
+const TOKENS = ["all", "USDC", "WETH", "cbETH", "BRETT"]
 
 export default function FilterBar({ activeToken, onSelect }) {
   return (
