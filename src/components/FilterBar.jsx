faa28c81efb986c304baccd45e18d0964c85a902
kimyee1 <neloezeagu@gmail.com>
2026-04-11 12:32:25 +0100

    Update FilterBar.jsx

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -3,7 +3,7 @@
 // e.g. clicking "USDC" shows only USDC whale trades
 
 // These are the filter options we show as buttons
-const TOKENS = ["all", "USDC", "WETH", "ETH" "USDT"]
+const TOKENS = ["all", "USDC", "WETH", "ETH", "USDT"]
 
 export default function FilterBar({ activeToken, onSelect }) {
   return (
